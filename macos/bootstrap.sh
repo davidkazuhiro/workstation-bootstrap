4c7ec8c9e4b701201cc3fbde7987ae5659e15185
David Somers-Harris <david.somers-harris@rakuten.com>
2017-10-26 13:51:12 +0900

    Update bootstrap.sh

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -1,1 +1,20 @@
+# Install Outlook
+# Install Wivia
+# Install Line
+# Install Synergy
+# Install chrome
+# Install Slack
+# Install Hipchat
+# Install Docker
+# Install homebrew
+# Install git (dev tools?)
+# Install Virtualbox
+# Install Vagrant
+# Install Chefdk
+# Install atom
+# Configure Terminal
+
+# Clone github repos
 mkdir -p ~/git/github/davidkazuhiro
+
+# Clone ghe repos
