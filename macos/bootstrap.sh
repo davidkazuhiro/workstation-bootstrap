4c8dc61db44cc35eea39748da8efc0e818eb3743
David Somers-Harris <david@somers-harris.com>
2018-10-26 14:28:27 +0900

    added ghe and bitbucket

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -57,12 +57,11 @@ clone_repos() {
   mkdir -p "$HOME/git/github/$1"
   while read repo; do
     git clone $(echo ${repo} | cut -d ' ' -f 2) $HOME/git/github/${1}/$(echo ${repo} | cut -d ' ' -f 1)
-  done < <(curl -s "https://api.github.com/${1}/${2}/repos?per_page=200" | jq -r '.[] | .name + " " + .ssh_url')
+  done < <(curl -s "https://api.github.com/${1}s/${2}/repos?per_page=200" | jq -r '.[] | .name + " " + .ssh_url')
 }
 
-clone_repos users davidkazuhiro
-clone_repos orgs rakutentech
-
+clone_repos user davidkazuhiro
+clone_repos org rakutentech
 
 # GHE
 ## Add public key to GitHub
@@ -70,7 +69,27 @@ clone_repos orgs rakutentech
 ## Clone all repos
 mkdir -p ~/git/ghe
 
+clone_repos() {
+  mkdir -p "$HOME/git/ghe/$1"
+  while read repo; do
+    git clone $(echo ${repo} | cut -d ' ' -f 2) $HOME/git/ghe/${1}/$(echo ${repo} | cut -d ' ' -f 1)
+  done < <(curl -s "https://ghe.rakuten-it.com/api/v3/${1}s/${2}/repos?per_page=200" | jq -r '.[] | .name + " " + .ssh_url')
+}
+
+clone_repos user david-somers-harris
+
 # BitBucket
 ## Do public keys work?
+
+## NOT TESTED
+clone_repos() {
+  mkdir -p "$HOME/git/ghe/$1"
+  while read repo; do
+    git clone $(echo ${repo} | cut -d ' ' -f 2) $HOME/git/ghe/${1}/$(echo ${repo} | cut -d ' ' -f 1)
+  done < <(curl -s "https://git.rakuten-it.com/rest/api/1.0/${1}s/${2}/repos?per_page=200" | jq -r '.[].values | .slug + " " + \(.links.clone[] | select(.name == "ssh" | .href)')
+}
+
+clone_repos project DSH
+clone_repos user david.somers-harris
 
 # Enable Japanese Input
