8b95b956e861d7ca8ba83006cc70eda54bd644a1
David Somers-Harris <david@somers-harris.com>
2018-12-11 09:17:18 +0900

    Update bootstrap.sh

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -6,66 +6,34 @@
 # Outlook
 # Wivia
 
-# homebrew
+# Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
 
-# Install laspass-cli
-brew install lastpass-cli
+# Generate SSH Keys
+ssh-keygen -t ed25519
 
-# Install Synergy
-brew cask install synergy
+# Add Key to GitHub
+read -n 1 -s -r -p "Add SSH public key to GitHub, then press any key to continue"
 
-# Secret Management
-brew cask install gpg-suite
-
-# Install Browsers
-brew cask install google-chrome
-brew cask install firefox
-
-# Chat
-brew cask install hipchat
-brew cask install slack
-brew cask install microsoft-teams
-brew cask install gitter
-
-# git
-brew install git
-
-# Virtualization Tools
-brew cask install vmware-fusion
-brew cask install virtualbox
-brew cask install vagrant
-brew install terraform
-brew cask install docker
-brew cask install hab
-
-# Network Tools
-brew cask install wireshark
-
-
-# Editors
-brew cask install visual-studio-code
-
-# Configure Terminal
-
-# SSH
-## Generate Keys
 ## Add config for login servers
 
 # GitHub
 ## Add public key to GitHub
 
+brew install jq
+
 ## Clone all repos
 clone_repos() {
   mkdir -p "$HOME/git/github/$1"
   while read repo; do
-    git clone $(echo ${repo} | cut -d ' ' -f 2) $HOME/git/github/${1}/$(echo ${repo} | cut -d ' ' -f 1)
+    git clone $(echo ${repo} | cut -d ' ' -f 2) $HOME/git/github/${2}/$(echo ${repo} | cut -d ' ' -f 1)
   done < <(curl -s "https://api.github.com/${1}s/${2}/repos?per_page=200" | jq -r '.[] | .name + " " + .ssh_url')
 }
 
 clone_repos user davidkazuhiro
 clone_repos org rakutentech
+
+# Configure Terminal
 
 # GHE
 ## Add public key to GitHub
@@ -87,7 +55,7 @@ clone_repos user david-somers-harris
 
 ## NOT TESTED
 clone_repos() {
-  mkdir -p "$HOME/git/ghe/$1"
+  mkdir -p "$HOME/git/bitbucket/$1"
   while read repo; do
     git clone $(echo ${repo} | cut -d ' ' -f 2) $HOME/git/ghe/${1}/$(echo ${repo} | cut -d ' ' -f 1)
   done < <(curl -s "https://git.rakuten-it.com/rest/api/1.0/${1}s/${2}/repos?per_page=200" | jq -r '.[].values | .slug + " " + \(.links.clone[] | select(.name == "ssh" | .href)')
